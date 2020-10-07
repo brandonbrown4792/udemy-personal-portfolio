3558094408bb0529b307b4fbdad6ce1418756a5a
Brandon Brown <hoobie4792@yahoo.com>
2020-10-07 00:47:22 -0400

    Added blog app to Django

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Steps to do add new model:
+
+# 1. Create class with corresponding name inheriting from models.Model
+# 2. Create variables inside class using models.<Field>
+# 3. Add app to settings file ('<app_name>.apps.<AppnameConfig>'). You can see this in apps.py for your respective model
+# 4. Create migration with python manage.py makemigrations
+# 5. Migrate with python manange.py migrate
+# 6. Add new model to admin inside admin.py file with admin.site.register(<ClassName>) if you want to have admin database functionality
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images')
--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+# Steps to do add new model:
+
+# 1. Create class with corresponding name inheriting from models.Model
+# 2. Create variables inside class using models.<Field>
+# 3. Add app to settings file ('<app_name>.apps.<AppnameConfig>'). You can see this in apps.py for your respective model
+# 4. Create migration with python manage.py makemigrations
+# 5. Migrate with python manange.py migrate
+# 6. Add new model to admin inside admin.py file with admin.site.register(<ClassName>) if you want to have admin database functionality
+
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
